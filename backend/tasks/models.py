f68ed1d2796f0ab3c4c0ed307067ddd8850378c2
Dan Williams <danielwilliams497@gmail.com>
2025-04-08 19:47:26 +0100

    Added task model
    Added task model, created migration, registered model, fixed dockerfile to follow var convention

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+
+
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        LOW = "Low"
+        MEDIUM = "Medium"
+        HIGH = "High"
+
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
+    title = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    completed_at = models.DateTimeField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    due_date = models.DateTimeField(null=True, blank=True)
+    priority = models.CharField(max_length=16, choices=Priority.choices, default=Priority.MEDIUM)
